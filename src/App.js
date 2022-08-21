926a921adbc91b586af877eac10c1c2f1cdac27e
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2022-08-21 23:29:16 +0700

    deploy

---
+++ src/App.js
@@ -0,0 +1,192 @@
+import logo from './logo.svg';
+import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import bg from '../src/assets/bg.png';
+import img1 from '../src/assets/55+ ảnh nền điện thoại cute dành cho fan của “We Bare Bears”.jpg';
+import img2 from '../src/assets/Wallpaper urso.jpg';
+import img3 from '../src/assets/55+ ảnh nền điện thoại cute dành cho fan của “We Bare Bears”.jpg';
+import img4 from '../src/assets/Blend ๛Ňɠʉүễŋ☂Áŋɦツ🍒.jpg';
+import img5 from '../src/assets/Samurai Jack entra para o catálogo do HBO Max.jpg';
+import img6 from '../src/assets/Imágenes Aesthetic Para Tu Cuenta.jpg';
+import img7 from '../src/assets/𝓲𝓬𝓮 𝓫𝓮𝓪𝓻.jpg';
+import img8 from '../src/assets/download.jpg';
+import img9 from '../src/assets/293617759_1203492253533961_5068474013380560787_n.png';
+function App() {
+  return (
+    <div className="App">
+    
+      <Carousel>
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Webpage được làm ra với mục đích chúc mừng sinh nhật Nguyễn Thu Trang nên là nhớ xem đến cuối nhé ❤️💕</p>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Chúc Trang tuổi mới ...</p>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Hay ăn chóng lớn ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img1}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Học tập thật tốt ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img2}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Mạnh khỏe hơn voi ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img3}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Nhan sắc thượng thừa ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img4}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Công việc thuận lợi ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img5}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Hạnh phúc vui tươi ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img6}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Tình cảm dồi dào ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img7}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Happy birthday (Khúc này bí văn quá) ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img8}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="cr-item">
+          <div className='d-flex flex-column align-items-center container justify-content-center' style={{height: 929}}>
+              <p className='text-white' style={{fontSize:'50px'}}>Sản phẩm được lên ý tưởng và thực hiện bằng cả tấm lòng bởi Nguyễn Anh Dũng ❤️💕</p>
+              <div className='rounded-circle' style={{height: 250, width: 250, border: '5px solid white'}}>
+                <img 
+                  src={img9}
+                  alt="First slide"
+                  className="responsive rounded-circle"
+                  style={{objectFit: 'cover', aspectRatio: '1/1'}}
+                />
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      
+      </Carousel>
+    </div>
+  );
+}
+
+export default App;
